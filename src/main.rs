ae566ac7262b6f3e1849dbf93fbf0fa7a7a34454
Pulsar33550336 <pulsar33550336@163.com>
2025-12-16 17:55:10 +0800

    feat: ???

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use crate::fun::Philia093Args;
 use crate::generate::GenArgs;
 use crate::ren::RenArgs;
+use clap::ArgAction;
 use clap::{Parser, Subcommand};
 use clap_i18n_richformatter::clap_i18n;
 use log::info;
 
 mod config;
 mod context;
+mod fun;
 mod generate;
 mod init;
 mod ren;
@@ -16,30 +19,35 @@ mod ren;
 struct Cli {
     #[command(subcommand)]
     pub command: Commands,
-    #[arg(short, long, global = true)]
+    #[arg(short, long, global = true, action = ArgAction::Count)]
     /// 详细模式
-    verbose: bool,
+    verbose: u8,
 }
 
 #[derive(Subcommand, Debug)]
+#[command(infer_subcommands = false)]
 enum Commands {
     /// 渲染题面
     Ren(RenArgs),
     /// 生成工程文件夹
     Gen(GenArgs),
+    /// see you *tomorrow*
+    #[command(hide = true, name = "PhiLia093")]
+    Philia093(Philia093Args),
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse_i18n_or_exit();
     // let cli = Cli::parse();
 
-    init::init(&cli.verbose)?;
+    init::init(&(cli.verbose >= 1))?;
 
     info!("booting up");
 
     let result = match cli.command {
         Commands::Ren(args) => ren::main(args),
         Commands::Gen(args) => generate::main(args),
+        Commands::Philia093(args) => fun::main(args, &cli.verbose),
     };
 
     if let Err(e) = result {
